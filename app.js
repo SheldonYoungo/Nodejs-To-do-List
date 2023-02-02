2d4e978c579ae1eda4b3861ca157864da3c98385
SheldonYoungo <sheldomyoungo@gmail.com>
2023-02-02 12:05:36 -0400

    Add get and post request

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.urlencoded({extend: true}));
+
 app.set('view engine', 'ejs'); // App view engine settled to use ejs
 
 app.get('/', (req, res) => {
-
     let today = new Date();
-    let day = new Date().toLocaleDateString('en-us', {weekday: 'long'});
+    let options = {weekday: 'long', day: 'numeric', month: 'long'};
+    let day = today.toLocaleDateString('en-us', options)
     
     res.render('list', {kindOfDay: day});
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.newTask);
 })
 
 app.listen(port, () => {
