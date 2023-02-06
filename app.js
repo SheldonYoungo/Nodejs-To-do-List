4b2cc6e7eda6083269bf8d60ce50442567eddee0
SheldonYoungo <sheldomyoungo@gmail.com>
2023-02-06 17:08:20 -0400

    Added delete item functionality

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ const app = express();
 
 app.use(express.urlencoded({extend: true}));
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs'); //Here we say that view engine should be replaced with ejs (which is to reduce the repeated things like html files)
 
 app.get('/', (req, res) => {
-    
     /* 
         In this case, the response renders the newListItem with the task array empty before the post request because if we try to render it within the post request, 
        the page will try to render it with a nothing in it, sending an error (note that the get request method triggers when we open the page and the post method when a form is submitted, 
@@ -26,23 +25,20 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     let item = req.body.newTask;
+    const btnValue = req.body.button;
+    
+    if(item !== '' &&  btnValue === 'submit') {
+        
+        tasks.push(item);
+        res.redirect('/'); //When the form is submitted, the page redirects the page to the home route again to render the text saved in the tasks array
+        
+    } else if(item !== '' && btnValue === 'delete') {
+        
+        tasks.splice( -1, 1);
+        res.redirect('/');
 
-    tasks.push(item);
-    res.redirect('/'); //When the form is submitted, the page redirects the page to the home route again to render the text saved in the tasks array
+    }
 })
-
-// Work list request
-app.get('/work', (req, res) => {
-    res.render('list', {listTitle: 'Work list', newListItems: workTasks, route: req.url});
-});
-
-app.post('/work', (req, res) => {
-    let workItem = req.body.newTask;
-
-    workTasks.push(workItem);
-
-    res.redirect('/work');
-});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
