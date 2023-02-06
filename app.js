9f5bb67743ecb777d0ca0c207ced7f74d89dcf97
SheldonYoungo <sheldomyoungo@gmail.com>
2023-02-06 13:47:34 -0400

    Added new ejs header and footer layouts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = 3000;
 let tasks = []; 
+let workTasks = [];
 
 const app = express();
 
@@ -20,16 +21,29 @@ app.get('/', (req, res) => {
        the page will try to render it with a nothing in it, sending an error (note that the get request method triggers when we open the page and the post method when a form is submitted, 
        so their scopes are different).
     */  
-    res.render('list', {kindOfDay: day, newListItems: tasks}); 
-})
+    res.render('list', {listTitle: day, newListItems: tasks, route: req.url}); 
+});
+
 
 app.post('/', (req, res) => {
-    item = req.body.newTask;
+    let item = req.body.newTask;
 
     tasks.push(item);
-
     res.redirect('/'); //When the form is submitted, the page redirects the page to the home route again to render the text saved in the tasks array
 })
+
+// Work list request
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: 'Work list', newListItems: workTasks, route: req.url});
+});
+
+app.post('/work', (req, res) => {
+    let workItem = req.body.newTask;
+
+    workTasks.push(workItem);
+
+    res.redirect('/work');
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
