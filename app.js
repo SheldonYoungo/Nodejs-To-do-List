f9dfc0cc4885d768e8928ebb801a495a93ae127d
SheldonYoungo <sheldomyoungo@gmail.com>
2023-04-10 10:07:41 -0400

    Fixed grammatical errors in comments

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs'); //Here we say that view engine should be replaced
 app.get('/', (req, res) => {
     /* 
         In this case, the response renders the newListItem with the task array empty before the post request because if we try to render it within the post request, 
-       the page will try to render it with a nothing in it, sending an error (note that the get request method triggers when we open the page and the post method when a form is submitted, 
+       the page will try to render it with nothing in it, sending an error (note that the get request method triggers when we open the page and the post method when a form is submitted, 
        so their scopes are different).
     */  
     res.render('list', {listTitle: getDate(), newListItems: tasks, route: req.url}); 
@@ -30,7 +30,7 @@ app.post('/', (req, res) => {
     if(item !== '' &&  btnValue === 'submit') {
         
         tasks.push(item);
-        res.redirect('/'); //When the form is submitted, the page redirects the page to the home route again to render the text saved in the tasks array
+        res.redirect('/'); //When the form is submitted, the page redirects to the home route again to render the text saved in the tasks array
         
     } else if(item !== '' && btnValue === 'delete') {
         
