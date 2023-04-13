7266df362b7b1ffaf3c6b864ffbe3e66ea70bbb0
SheldonYoungo <sheldomyoungo@gmail.com>
2023-04-13 11:10:35 -0400

    Added project to repository

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import { getDate } from './date.js';
 import  express  from 'express';
-
-const port = 3000;
-let tasks = []; 
-let workTasks = [];
+import mongoose  from 'mongoose';
 
 const app = express();
 
@@ -11,33 +8,94 @@ app.use(express.urlencoded({extend: true}));
 
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs'); //Here we say that view engine should be replaced with ejs (which is to reduce the repeated things like html files)
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    /* 
-        In this case, the response renders the newListItem with the task array empty before the post request because if we try to render it within the post request, 
-       the page will try to render it with nothing in it, sending an error (note that the get request method triggers when we open the page and the post method when a form is submitted, 
-       so their scopes are different).
-    */  
-    res.render('list', {listTitle: getDate(), newListItems: tasks, route: req.url}); 
+const port = 3000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB')
+
+const itemSchema = {
+    name: { type: String, required: true}
+};
+
+const Item = mongoose.model('Item', itemSchema);
+
+const item1 = new Item({
+    name: 'Welcome to your to do list!'
 });
 
+const item2 = new Item({
+    name: 'Hit the + button to add a new item'
+});
 
-app.post('/', (req, res) => {
-    let item = req.body.newTask;
-    const btnValue = req.body.button;
+const item3 = new Item({
+    name: '<--- Hit the x button to delete an item'
+})
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+
+const List = mongoose.model('List', listSchema);
+
+const defaultItems = [item1, item2, item3];
+
+app.get('/', async (req, res) => {
     
-    if(item !== '' &&  btnValue === 'submit') {
-        
-        tasks.push(item);
-        res.redirect('/'); //When the form is submitted, the page redirects to the home route again to render the text saved in the tasks array
-        
-    } else if(item !== '' && btnValue === 'delete') {
-        
-        tasks.splice( -1, 1);
-        res.redirect('/');
+    let foundItems = await Item.find({});
 
+    if(foundItems.length === 0) {
+        Item.insertMany(defaultItems)
+        .then(() => console.log('Succesfully saved!'))
+        .catch(err => console.log(err))
+
+        res.redirect('/')
+    } else {
+
+        res.render('list', {listTitle: getDate(), newListItems: foundItems, route: req.url}); 
     }
+});
+
+app.get('/:customRouteName', (req, res) => {
+    const customRouteName = req.params.customRouteName;
+
+    List.findOne({name: customRouteName})
+        .then(result => {
+            if(result === null){
+                const list = new List({
+                    name: customRouteName,
+                    items: defaultItems
+                });
+
+                list.save();
+
+                res.render('list', {listTitle: list.name, newListItems:list.items, route: req.url})
+            } else {
+                res.render('list', {listTitle: result.name, newListItems:result.items, route: req.url})
+            }
+        })
+        .catch(err => console.log(err))
+});
+
+app.post('/', async (req, res) => {
+    const itemName = req.body.newTask;
+
+    const task = new Item({
+        name: itemName
+    });
+    
+    task.save();
+    res.redirect('/'); 
+})
+
+app.post('/delete', (req, res) => {
+    const itemID = req.body.checkbox;
+
+    Item.findByIdAndRemove(itemID)
+        .then(() => console.log('Item removed'))
+        .catch(err => console.log(err))
+        .finally(() => res.redirect('/'))
 })
 
 app.listen(port, () => {
